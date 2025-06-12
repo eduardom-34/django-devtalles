b721130c179e5dc48c1a402848010406a5316193
CesarLinux <eduardomelchor34@gmail.com>
2025-06-12 15:40:50 -0600

    Regresando un HTML

--- playground/quotes/urls.py
+++ playground/quotes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),
     path("<int:day>", views.days_week_with_number), #quotes/friday
     path("<str:day>", views.days_week, name="day-quote") #quotes/friday
 ]
--- playground/quotes/views.py
+++ playground/quotes/views.py
@@ -18,12 +18,24 @@ days_of_week = {
 def days_week_with_number(request, day):
     days = list(days_of_week.keys())
     if day > len(days):
-        return HttpResponseNotFound("El dia no existe")
+        return HttpResponseNotFound("<h1>El dia no existe</h1>")
     redirect_day = days[day - 1]
     
     redirect_path = reverse("day-quote", args=[redirect_day])
     return HttpResponseRedirect(redirect_path)
 
+def index(request):
+    list_items = ""
+    days = list(days_of_week.keys()) #[monday, tuesday, wednesday, thursday, friday, saturday, sunday]
+    
+    for day in days:
+        day_path = reverse("day-quote", args=[day])
+        list_items += f"<li><a href=\"{day_path}\">{day}</a></li>"
+        
+    response_html = f"<ul>{list_items}</ul>"
+    
+    return HttpResponse(response_html)
+    
 
 def days_week(request, day):
     
