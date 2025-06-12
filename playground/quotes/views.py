b671ca7b363f98928f1da4ec75ddecaf0f890adf
CesarLinux <eduardomelchor34@gmail.com>
2025-06-12 14:43:10 -0600

    Simplificando la logica de forma dinamica

--- playground/quotes/views.py
+++ playground/quotes/views.py
@@ -2,20 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+days_of_week = {
+    "monday": "Pienso, luego existo",
+    "tuesday": "La vida es un sueño",
+    "wednesday": "El conocimiento es poder",
+    "thursday": "Se el cambio que quieres ver en el mundo",
+    "friday": "Solo se que no se nada",
+    "saturday": "Vive como si fuera el ultimo dia", 
+    "sunday": "Da un poquito mas todo los dias"
+    
+}
+
 def days_week_with_number(request, day):
     return HttpResponse(f"{day}")
 
 
 def days_week(request, day):
     
-    quote_text = None
+    try:
+        quote_text = days_of_week[day]
+        return HttpResponse(quote_text)
+    except:
+        return HttpResponseNotFound("Este dia no existe")
+        
     
-    if day == "monday":
-        quote_text = "Pienso, luego existo"
-    elif day == "tuesday":
-        quote_text = "La vida es un sueño"
-    else:
-        return HttpResponseNotFound("No hay frases para este dia")
-    
-    return HttpResponse(quote_text)
         
